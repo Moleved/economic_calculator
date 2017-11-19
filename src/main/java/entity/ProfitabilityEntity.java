655854b64032da4060e0dd6ebdc4caca3763113f
Jack Molosh <jackmolosh@gmail.com>
2017-11-19 21:33:33 +0300

    Update profitability entity with use hibernate annotation

--- src/main/java/entity/ProfitabilityEntity.java
+++ src/main/java/entity/ProfitabilityEntity.java
@@ -1,8 +1,22 @@
 package entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="profitability")
 public class ProfitabilityEntity {
+    @Id @GeneratedValue
+    @Column(name="id")
     private int id;
+
+    @Column(name="profit_from_all_activities")
     private Double profitFromAllActivities;
+
+    @Column(name="total_product_sales_costs")
+    private Double totalProductSalesCosts;
+
+    @Column(name="result")
+    private Double result;
 
     public int getId() {
         return id;
@@ -18,6 +32,22 @@ public class ProfitabilityEntity {
 
     public void setProfitFromAllActivities(Double profitFromAllActivities) {
         this.profitFromAllActivities = profitFromAllActivities;
+    }
+
+    public Double getTotalProductSalesCosts() {
+        return totalProductSalesCosts;
+    }
+
+    public void setTotalProductSalesCosts(Double totalProductSalesCosts) {
+        this.totalProductSalesCosts = totalProductSalesCosts;
+    }
+
+    public Double getResult() {
+        return result;
+    }
+
+    public void setResult(Double result) {
+        this.result = result;
     }
 
     @Override
